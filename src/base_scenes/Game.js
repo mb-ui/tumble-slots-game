cf4c7058f1fc61ce7fd14e2d23f3250459f6a9e3
mb-ui <biglary.mohsen.new69@gmail.com>
2025-08-11 05:13:46 +0330

    rename base_classes folder into component

--- src/base_scenes/Game.js
+++ src/base_scenes/Game.js
@@ -1,14 +1,14 @@
 import Config from '../config';
 import Options from '../options';
 //import Class
-import Audio from '../base_classes/Audio';
-import Sprite from '../base_classes/Sprite';
-import Slots from '../base_classes/slots/slots.factory';
-import PayTable from '../base_classes/payTable/payTable.factory';
-import Maxbet from '../base_classes/Maxbet';
-import SpinButton from '../base_classes/spinButton/spinButton.factory';
-import CreditBoard from '../base_classes/creditBoard/creditBoard.factory';
-import ScoreBoard from '../base_classes/scoreBoard/scoreBoard.factory';
+import Audio from '../components/Audio';
+import Sprite from '../components/Sprite';
+import Slots from '../components/slots/slots.factory';
+import PayTable from '../components/payTable/payTable.factory';
+import Maxbet from '../components/Maxbet';
+import SpinButton from '../components/spinButton/spinButton.factory';
+import CreditBoard from '../components/creditBoard/creditBoard.factory';
+import ScoreBoard from '../components/scoreBoard/scoreBoard.factory';
 export default class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'Game' });
